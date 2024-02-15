cce3ad7ed624ca1c59be4e66203cded2244b4e73
Hareesh Naik <hareeshnaik836@gmail.com>
2024-02-15 11:48:01 +0530

    updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Profile from '../assets/profile.png';
+import profile from '../assets/myimg.jpg';
 import {Styles} from '../utils/Style.js';
 import Resume from '../assets/Hareesh_Resume.pdf';
+import {TiArrowDownOutline} from "react-icons/ti";
+import '../App.css'
 
 
 function About() {
@@ -9,7 +11,7 @@ function About() {
     <div className='flex flex-wrap justify-center items-center mt-10 p-10' id='Home'>
       <div className='flex-1 py-4'>
         <span className={Styles.heroHeadText}>
-          Hi,I'm <span className='text-orange-600'>
+          Hi,I'm <span className='text-orange-600 '>
 Hareesh Naik
           </span>
           <br/>
@@ -18,6 +20,22 @@ Hareesh Naik
 
           </span>
         </span>
+
+        <br/>
+        <div className='w-[14rem'>
+            <a  href={Resume} download="Resume" target='blank' rel='noreferrer'>
+              <div className='p-4 mt-2 w-[14rem] rounded-xl text-orange-500 flex justify-center border-2 border-orange-500 hover:border-violet-500  shadow-lg hover:shadow-pink-500' ><TiArrowDownOutline className='text-lg mr-3 mt-1 text-orange-500 '>
+
+              </TiArrowDownOutline>
+                 Resume
+              </div>
+            </a>
+        </div>
+
+      </div>
+
+      <div className='flex flex-wrap '>
+        <img className='img'  src={profile} alt='profile'/>
       </div>
     </div>
   )
