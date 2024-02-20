afdcb24b8f68887001645c365666dd14be796bd0
Hareesh Naik <hareeshnaik836@gmail.com>
2024-02-20 14:51:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
 
       <Contact/>
 
+
       <Footer/>
 
     </div>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Styles } from '../utils/Style'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_bkilbna', 'template_vd5p7jr', form.current,
+     'pI-uUDBnS3vnOOnFu'
+          )
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...');
+            },
+          );
+          e.target.reset()
+      };
+
   return (
     <div>
         <div className='flex-row items-center pl-3 justify-center pt-3' id='Contact'>
@@ -15,7 +36,7 @@ function Contact() {
             <div className='flex flex-wrap flex-row justify-around'>
                 <div className='lg:w-[40%]'>
 
-                    <form className='m-5 flex flex-col pag-3'>
+                    <form ref={form} onSubmit={sendEmail} className='m-5 flex flex-col pag-3'>
                        
                         <label className='flex flex-col gap-3'>
                             <span className='text-violet-700 font-medium mb-2'>
@@ -23,9 +44,9 @@ function Contact() {
                             </span>
                             <input
                             type='text'
-                            name='name'
+                            name='user_name'
                             placeholder='Whats your good name?'
-                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium' >
+                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium' required>
                             
                             </input>
                         </label>
@@ -36,9 +57,9 @@ function Contact() {
                             </span>
                             <input
                             type='email'
-                            name='email'
+                            name='user_email'
                             placeholder='Enter your Email'
-                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium' >
+                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium' required >
                             
                             </input>
                         </label>
@@ -49,9 +70,9 @@ function Contact() {
                             </span>
                             <textarea
                             row={5}
-                            name='text'
+                            name='message'
                             placeholder='Whats your message'
-                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium' >
+                            className='bg-orange-200 py-4 px-6 placeholder:text-secondary text-orange-500 rounded-lg outline-none border-none font-medium'required >
                             
                             </textarea>
                         </label>
